99204bf955075b2c79378715ccb73125d03ded61
NotVishesh <vishesh.bhu1971@gmail.com>
2024-07-27 22:15:27 +0530

    made  utils

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import exress from 'exress'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+
+const app = exress();
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(exress.json({limit:"16kb"}))
+
+app.use(express.urlencoded({extended : true ,limit : '16kb'}))
+
+app.use(exress.static('public'))
+
+app.use(cookieParser())
+
+export {app}
