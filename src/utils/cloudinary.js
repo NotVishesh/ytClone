2a67e28913e9baf879964f2d1002b97ec5527774
NotVishesh <vishesh.bhu1971@gmail.com>
2024-08-06 16:54:41 +0530

    lean basic routes handaling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary} from "cloudinary";
+
+import fs from 'fs';
+
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret:process.env.CLOUDINARY_API_SECRET  
+});
+
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+
+        //upload file on cloudinary
+       const response = await cloudinary.uploader.upload(localFilePath,
+            {
+                resoure_type:"auto"
+            }
+        )
+        //file has bein uploaded successfully
+        console.log('File uploaded successfully on cloudinary and its url is', response.url);
+        return response
+    }catch(errq){
+        fs.unlinkSync(localFilePath)  //remove the locally saved temp file as the upload operation get failed
+    }
+}
+
+
+export {uploadOnCloudinary}
