68e1d0b04330d7ea4ad16eeba87e16285ad90b75
NotVishesh <vishesh.bhu1971@gmail.com>
2024-08-07 22:00:19 +0530

    learing how to get the data from the user in the form of body

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,32 @@
+import { apiError } from '../utils/apiError.js';
 import {asyncHandler} from '../utils/asyncHandler.js'
 
 const registerUser = asyncHandler(async (req ,res) => {
-    res.status(200).json({
-        message : 'ok'
-    })
+    //TODO
+    // get user info from frontend 
+    // validation - not empty
+    //check if user already exits
+    //check for images and, check avatar
+    // upload them to cloudinary
+    // create a user opbject - create entry in db
+    // remove password and ref token feild from the responce
+    // check if the user is created 
+    // return response 
+
+
+
+    // getting user details
+    const {fullName , email, username , password} = req.body
+    console.log(`${fullName} ${email} ${username} ${password} `);
+
+
+    //validation
+    if ([fullName,email,username,password].some((field)=>{
+        return field?.trim() ==="";
+    })) {
+        throw new apiError (400, "All fields are require")
+    }
+    
 })
 
 
